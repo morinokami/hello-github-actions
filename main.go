a0cd40212b54cf56f5490a3e182799bf43721535
Shinya Fujino <shf0811@gmail.com>
2019-11-27 22:15:17 +0900

    Add Chinese greeting (tests should fail)

--- main.go
+++ main.go
@@ -6,11 +6,23 @@ import (
 	"os"
 )
 
+// Lang represents the language used to greet.
+type Lang int
+
+// En: English, Zh: Chinese
+const (
+	En Lang = iota
+	Zh
+)
+
 // Hello greets GitHub Actions.
-func Hello(out io.Writer) {
+func Hello(out io.Writer, lang Lang) {
+	if lang == Zh {
+		fmt.Fprint(out, "你好, GitHub Actions")
+	}
 	fmt.Fprint(out, "Hello, GitHub Actions!")
 }
 
 func main() {
-	Hello(os.Stdout)
+	Hello(os.Stdout, En)
 }
--- main_test.go
+++ main_test.go
@@ -6,13 +6,36 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	Hello(buffer)
 
-	got := buffer.String()
-	want := "Hello, GitHub Actions!"
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Hello in English", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		lang := En
+
+		Hello(buffer, lang)
+
+		got := buffer.String()
+		want := "Hello, GitHub Actions!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Chinese", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		lang := Zh
+
+		Hello(buffer, lang)
+
+		got := buffer.String()
+		want := "你好, GitHub Actions!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
